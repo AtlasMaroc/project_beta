2b8ab023479a61e11624431b8dcbd61a06ce5379
AtlasMaroc <moha.castellajo2002@gmail.com>
2024-04-18 15:25:33 +0200

    pipeline for qualtiy assement

--- pipeline.sh
+++ pipeline.sh
@@ -7,7 +7,7 @@ pack=$( conda info | awk -F' : ' 'NR==2 {print $2, exit}')
 if ! [[ $pack == assembly ]]
     then 
 	    echo "Please activate the conda environement and assembly first"
-     
+            exit 1
 fi
 
 #check for the presence of arguments 
@@ -22,11 +22,25 @@ fi
 
 echo "platform, length" >length.csv
 
-#iterate over files in path to directory provided as an argument 
+#iterate over files in a directory providing it path as an argument 
+
 
 for file in $1
 do
 	bioawk -c fastx '{print $name, length($seq)}' $file >>length.csv 
 done 
 
-#visualizing read length distrubition data using r script:	
+#visualizing read length distrubition data using r script:
+
+./readlenght.R
+
+#calculating N50 statstics using assembly stats:
+
+#calculting N50 statistics for ever file:
+
+for files in $1
+do 
+	assembly-stats $1 >>N50_stats
+done 
+
+
